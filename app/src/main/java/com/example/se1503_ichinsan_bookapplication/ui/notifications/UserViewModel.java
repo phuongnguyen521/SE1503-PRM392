48a3efe261c15704b62472aea4df8733dfe4dfa8
phuongnguyen521 <ngcphungnguyn@gmail.com>
2022-06-21 05:34:58 +0700

    First commit

---
+++ app/src/main/java/com/example/se1503_ichinsan_bookapplication/ui/notifications/UserViewModel.java
@@ -0,0 +1,19 @@
+package com.example.se1503_ichinsan_bookapplication.ui.notifications;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class UserViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public UserViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is user fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
